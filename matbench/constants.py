4c9a84081ebbead2bf22ce5fa4d2eea3f3cc0cdc
ardunn <ardunn@lbl.gov>
2021-02-27 21:02:22 -0800

    add validation config as a constant

--- matbench/constants.py
+++ matbench/constants.py
@@ -4,7 +4,12 @@ import json
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATASETS_JSON_PATH = os.path.join(THIS_DIR, "datasets.json")
+VALIDATION_JSON_PATH = os.path.join(THIS_DIR, "validation.json")
 
 
 with open(DATASETS_JSON_PATH, "r") as f:
     DATASETS = json.load(f)
+
+
+with open(VALIDATION_JSON_PATH ,"r") as f:
+    VALIDATION = json.load(f)
