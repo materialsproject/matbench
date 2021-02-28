2bc11b125c3ae8484499adbb1dd27030d98fffa4
ardunn <ardunn@lbl.gov>
2021-02-27 21:05:29 -0800

    add test for validation metadata file

--- matbench/tests/test_data.py
+++ matbench/tests/test_data.py
@@ -4,11 +4,13 @@ import numpy as np
 from pymatgen import Structure
 
 from matbench.data import load
-from matbench.constants import DATASETS
+from matbench.constants import DATASETS, VALIDATION
+
+REG_KEY = "regression"
+CLF_KEY = "classification"
 
 
-class TestLoad(unittest.TestCase):
-
+class TestMetadata(unittest.TestCase):
     def test_datasets_metadata_file(self):
         for metadata in DATASETS.values():
             for key in ["problem_type", "n_samples", "input_type", "target"]:
@@ -18,11 +20,17 @@ class TestLoad(unittest.TestCase):
                     self.assertIn(metadata[key], ["composition", "structure"])
 
                 elif key == "problem_type":
-                    self.assertIn(metadata[key], ["regression", "classification"])
+                    self.assertIn(metadata[key], [REG_KEY, CLF_KEY])
         self.assertEqual(len(list(DATASETS.values())), 13)
 
-    def test_downloads(self):
+    def test_validation_metadata_file(self):
+        self.assertIn(REG_KEY, VALIDATION.keys())
+        self.assertIn(CLF_KEY, VALIDATION.keys())
 
+
+class TestLoad(unittest.TestCase):
+
+    def test_downloads(self):
         for dataset, metadata in DATASETS.items():
             print(f"Testing {dataset} with metadata {metadata}")
             df = load(dataset)
@@ -32,8 +40,10 @@ class TestLoad(unittest.TestCase):
 
             input_type = metadata["input_type"]
             test_type = Structure if input_type == "structure" else str
-            self.assertTrue(all([isinstance(input, test_type) for input in df[metadata["input_type"]]]))
+            self.assertTrue(all([isinstance(input, test_type) for input in
+                                 df[metadata["input_type"]]]))
 
             problem_type = metadata["problem_type"]
-            test_types = [np.bool_, bool] if problem_type == "classification" else [np.float_, np.float32, np.float64, float]
+            test_types = [np.bool_, bool] if problem_type == CLF_KEY else [
+                np.float_, np.float32, np.float64, float]
             self.assertIn(df[metadata["target"]].dtypes, test_types)
