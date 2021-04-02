43335dc29bd8d8f6c4793eb45c62ecf74b052f11
ardunn <ardunn@lbl.gov>
2021-04-02 14:41:22 -0700

    add benchmark submission test

---
+++ scripts/test_submission.py
@@ -0,0 +1,60 @@
+import os
+from unittest import TestCase
+
+from monty.serialization import loadfn
+
+from matbench.bench import MatbenchBenchmark
+
+
+BENCHMARKS_DIR = os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir), "benchmarks")
+
+# LOCAL = False
+LOCAL = True
+
+INFO_FILE = "info.json"
+NOTEBOOK_FILE = "notebook.ipynb"
+RESULTS_FILE = "results.json.gz"
+
+class BenchmarkSubmissionTest(TestCase):
+
+    def setUp(self) -> None:
+        print(f"Running test in benchmark dir: {BENCHMARKS_DIR}")
+        print(f"Running test locally? {LOCAL}")
+
+    def test_dir_structure_correct(self):
+        self.assertTrue(os.path.exists(BENCHMARKS_DIR))
+
+        # make sure the dir only has submissions as directories
+        for file_or_dir in os.listdir(BENCHMARKS_DIR):
+            full_path = os.path.join(BENCHMARKS_DIR, file_or_dir)
+            if not full_path.endswith(".DS_Store"):
+                self.assertTrue(os.path.isdir(full_path))
+
+            if not LOCAL:
+                for banned_file in (".ipynb_checkpoints",):
+                    self.assertFalse(os.path.exists(os.path.join(full_path, banned_file)))
+
+    def test_submissions(self):
+        for bmark_d in os.listdir(BENCHMARKS_DIR):
+            bmark_path = os.path.join(BENCHMARKS_DIR, bmark_d)
+            if os.path.isdir(bmark_path):
+                print(f"checking benchmark {bmark_path}")
+                for required_file in (INFO_FILE, RESULTS_FILE, NOTEBOOK_FILE):
+
+                    full_path = os.path.join(bmark_path, required_file)
+                    print(full_path)
+                    self.assertTrue(os.path.exists(full_path))
+
+                    if required_file == INFO_FILE:
+                        info = loadfn(full_path)
+                        self.assertIn("notes", info)
+
+                        for field_must_not_be_empty in ("authors", "algorithm", "algorithm_long", "bibtex_refs"):
+                            self.assertTrue(info.get(field_must_not_be_empty))
+
+                    if required_file == RESULTS_FILE:
+                        mb = MatbenchBenchmark.from_file(full_path)
+                        is_valid = mb.is_valid
+                        self.assertTrue(is_valid)
+
+                        print(f"{RESULTS_FILE} file in {bmark_path}: valid ({is_valid}), complete ({mb.is_complete}), recorded ({mb.is_recorded})")
