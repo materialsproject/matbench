676e3390b297c5b69d809d01f0a70eac96f47d17
ardunn <ardunn@lbl.gov>
2021-04-10 14:06:51 -0700

    working doc builder for individual task leaderboard pages

--- scripts/rebuild_docs.py
+++ scripts/rebuild_docs.py
@@ -7,17 +7,24 @@ from operator import gt, lt
 
 import tqdm
 from monty.serialization import loadfn
+import pandas as pd
+import plotly.express as px
 
-
+from matbench.task import MatbenchTask
 from matbench.bench import MatbenchBenchmark
 from matbench.constants import MBV01_KEY, CLF_KEY, REG_KEY
 from matbench.metadata import mbv01_metadata
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 DOCS_DIR = os.path.join(THIS_DIR, "../docs_src")
 STATIC_DOCS_DIR = os.path.join(DOCS_DIR, "static")
 BENCHMARKS_DIR = os.path.join(THIS_DIR, "../benchmarks")
 FULL_DATA_DIR = os.path.join(DOCS_DIR, "Full Benchmark Data")
+PER_TASK_DIR = os.path.join(DOCS_DIR, "Per-Task Leaderboards")
 
 
 def generate_scaled_errors_plot(all_data):
@@ -27,18 +34,148 @@ def generate_general_purpose_leaderboard(gp_leaderboard_data_by_bmark):
     pass
 
 def generate_per_task_leaderboards(task_leaderboard_data_by_bmark):
-    pass
+    for bmark_name, tasks_data in task_leaderboard_data_by_bmark.items():
+        for task, entries in tasks_data.items():
+
+            task_type = entries[0]["type"]
+
+            if task_type == REG_KEY:
+
+                table_data = {
+                    "algorithm": [],
+                    "algorithm w/ link": [],
+                    "mean mae": [],
+                    "std mae": [],
+                    "mean rmse": [],
+                    "max max_error": []
+                }
+            elif task_type == CLF_KEY:
+                table_data = {
+                    "algorithm": [],
+                    "algorithm w/ link": [],
+                    "mean rocauc": [],
+                    "std rocauc": [],
+                    "mean f1": [],
+                    "mean balanced_accuracy": []
+                }
+            else:
+                raise ValueError(f"Task type {task_type} not recognized!")
+
+            for entry in entries:
+                algo_name = entry["algorithm"]
+                link = entry["link"]
+                scores = entry["scores"]
+
+                table_data["algorithm w/ link"].append(f"[{algo_name}]({link})")
+                table_data["algorithm"].append(algo_name)
+
+                if task_type == REG_KEY:
+                    table_data["mean mae"].append(scores.mae.mean)
+                    table_data["std mae"].append(scores.mae.std)
+                    table_data["mean rmse"].append(scores.rmse.mean)
+                    table_data["max max_error"].append(scores.max_error.max)
+                else:
+                    table_data["mean rocauc"].append(scores.rocauc.mean)
+                    table_data["std rocauc"].append(scores.rocauc.std)
+                    table_data["mean f1"].append(scores.f1.mean)
+                    table_data["mean balanced_accuracy"].append(scores.balanced_accuracy.mean)
+
+            df = pd.DataFrame(table_data)
+            df = df.set_index("algorithm")
+            sorting_column = "mean rocauc" if task_type == CLF_KEY else "mean mae"
+            df = df.sort_values(by=sorting_column)
+
+            mbt = MatbenchTask(task, autoload=False, benchmark=bmark_name)
+
+            # header of the page
+            header = f"# {bmark_name} {task}\n\n"
+
+            subheader = f"## Individual Task Leaderboard for `{task}`\n\n"
+            explanation = "_Leaderboard for an individual task. Algorithms shown here may include " \
+                          "both general purpose and specialized algorithms (i.e., algorithms which " \
+                          "are only valid for a subset of tasks in the benchmark._\n\n"
+
+            info_header = "### Dataset info\n\n"
+
+            info_body = f"##### Description\n\n{mbt.metadata.description}\n\n"
+            info_body += f"Number of samples: {mbt.metadata.num_entries}\n\n"
+            info_body += f"Task type: {mbt.metadata.task_type}\n\n"
+            info_body += f"Input type: {mbt.metadata.input_type}\n\n"
+            info_body += f"##### Dataset columns\n\n" + "".join([f"- {c}: {cd}\n" for c, cd in mbt.metadata.columns.items()]) + "\n\n"
+            info_body += f"##### Dataset reference\n\n `{mbt.metadata.reference}`\n\n"
+
+            metadata_header = "### Metadata\n\n"
+            metadata = f"```\n{pprint.pformat(mbt.metadata)}\n```\n\n"
+
+            table_header = "### Leaderboard\n\n"
+            column_headers = df.columns
+            table = "| " + " | ".join(column_headers) + " |\n" +  "|------" * len(column_headers) + "|\n"
+            for ix, row in df.iterrows():
+                table += "| "
+                for th in column_headers:
+                    if th == "algorithm w/ link":
+                        table += f"{row[th]} | "
+                    else:
+                        number = format_float(row[th])
+                        if th == sorting_column:
+                            number = f"**{number}**"
+                        table += f"{number} | "
+                table += "\n"
+            table += "\n"
+
+            table = table.replace("algorithm w/ link", "algorithm")
+
+
+            droppables = ["mean f1", "mean balanced_accuracy"] if task_type == CLF_KEY else ["mean rmse", "max max_error"]
+            df = df.drop(columns=["algorithm w/ link"] + droppables)
+            error_metric = "std rocauc" if task_type == CLF_KEY else "std mae"
+            fig = px.scatter(df, error_y=error_metric, log_y=True)
+
+            metric = "MAE" if task_type == REG_KEY else "ROCAUC"
+
+            target = mbt.metadata.target
+            lower_or_higher = "lower" if task_type == REG_KEY else "higher"
+            title = f"Errors predicting '{target}' ({lower_or_higher} is better)"
+            fig.update_layout(title_text=title,
+                              title_font_size=15,
+                              showlegend=False,
+                              yaxis_title=metric,
+                              xaxis_title="", paper_bgcolor='rgba(0,0,0,0)',
+                              plot_bgcolor='rgba(0,0,0,0)',
+                              font={"color": "white"})
+            fig.update_yaxes(linecolor="grey", gridcolor="grey")
+            fig.update_xaxes(linecolor="rgba(0,0,0,0)", gridcolor="rgba(0,0,0,0)")
+
+            fig_path = f"task_{bmark_name}_{task}.html"
+            fig.write_html(os.path.join(STATIC_DOCS_DIR, fig_path))
+
+            fig_reference = f'\n<iframe src="/static/{fig_path}" class="is-fullwidth" height="700px" width="1000px" frameBorder="0"> </iframe>\n\n'
+
+            task_leaderboard_page = header + \
+                                    subheader + \
+                                    explanation + \
+                                    table_header + \
+                                    table + \
+                                    fig_reference + \
+                                    info_header + \
+                                    info_body + \
+                                    metadata_header + \
+                                    metadata
+            fname = os.path.join(PER_TASK_DIR, f"{bmark_name}_{task}.md")
+            with open(fname, "w") as f:
+                f.write(task_leaderboard_page)
+
+
 
 def organize_task_data(all_data):
     all_data_per_benchmark = {}
 
-    prefix = "Full%20Benchmark%20Data/"
-
+    prefix = "/Full%20Benchmark%20Data/"
 
     for data_packet in all_data.values():
         bmark_name = data_packet["results"].benchmark_name
         if bmark_name in all_data_per_benchmark:
-            all_data_per_benchmark.append(data_packet)
+            all_data_per_benchmark[bmark_name].append(data_packet)
         else:
             all_data_per_benchmark[bmark_name] = [data_packet]
 
@@ -112,9 +249,6 @@ def organize_task_data(all_data):
             task_leaderboards_data_by_bmark[bmark_name] = task_leaderboards
 
         return gp_leaderboard_data_by_bmark, task_leaderboards_data_by_bmark
-
-
-
 
 
 def generate_info_pages(all_data):
@@ -239,6 +373,7 @@ if __name__ == "__main__":
 
     gp_leaderboard_data_by_bmark, task_leaderboards_data_by_bmark = organize_task_data(all_data)
 
+    generate_per_task_leaderboards(task_leaderboards_data_by_bmark)
 
-    pprint.pprint(gp_leaderboard_data_by_bmark)
-    pprint.pprint(task_leaderboards_data_by_bmark)
+    # pprint.pprint(gp_leaderboard_data_by_bmark)
+    # pprint.pprint(task_leaderboards_data_by_bmark)
