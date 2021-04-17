16dc079eab8a557819c481fd5989e7da687b6daa
ardunn <ardunn@lbl.gov>
2021-04-17 00:17:05 -0700

    update info and src file for rf

--- benchmarks/matbench_v0.1_rf/run.py
+++ benchmarks/matbench_v0.1_rf/run.py
@@ -1,24 +1,38 @@
-# Example py file
+"""
+Code for training and recording the matbench_v0.1 random forest benchmark.
+
+The ML pipeline is placed within the Automatminer pipeline code infrastructure for convenience.
+
+All training and inference was done on a single 128-core HPC node.
+
+Reduce the number of jobs n_jobs for less memory usage on consumer machines.
+"""
 
 
 from automatminer import MatPipe
+from automatminer.featurization import AutoFeaturizer
 from automatminer.automl.adaptors import SinglePipelineAdaptor
+from automatminer.preprocessing import DataCleaner, FeatureReducer
+from automatminer.automl.adaptors import TPOTAdaptor, SinglePipelineAdaptor
 
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from matbench.bench import MatbenchBenchmark
 
 
 
+# The learner is a single 500-estimator Random Forest model
 learner = SinglePipelineAdaptor(
-                regressor=RandomForestRegressor(**learner_kwargs),
-                classifier=RandomForestClassifier(**learner_kwargs),
+                regressor=RandomForestRegressor(n_estimators=500),
+                classifier=RandomForestClassifier(n_estimators=500),
             )
 pipe_config = {
             "learner": learner,
-            "reducer": FeatureReducer(**reducer_kwargs),
-            "cleaner": DataCleaner(**cleaner_kwargs),
-            "autofeaturizer": AutoFeaturizer(**autofeaturizer_kwargs),
+            "reducer": FeatureReducer(reducers=[]),
+            "cleaner": DataCleaner(feature_na_method="mean", max_na_frac=0.01, na_method_fit="drop", na_method_transform="mean"),
+            "autofeaturizer": AutoFeaturizer(n_jobs=10, preset="debug"),
         }
+
+pipe = MatPipe(**pipe_config)
 
 mb = MatbenchBenchmark(autoload=False)
 
@@ -26,26 +40,20 @@ for task in mb.tasks:
     task.load()
     for fold in task.folds:
 
-        # Inputs are either chemical compositions as strings
-        # or crystal structures as pymatgen.Structure objects.
-        # Outputs are either floats (regression tasks) or bools (classification tasks)
-        train_inputs, train_outputs = task.get_train_and_val_data(fold)
+        df_train = task.get_train_and_val_data(fold, as_type="df")
 
-        # train and validate your model
-        my_model.train_and_validate(train_inputs, train_outputs)
+        # Fit the RF with matpipe
+        pipe.fit(df_train, task.metadata.target)
 
-        # Get testing data
-        test_inputs = task.get_test_data(fold, include_target=False)
+        df_test = task.get_test_data(fold, include_target=False, as_type="df")
+        predictions = pipe.predict(df_test)[f"{task.metadata.target} predicted"]
 
-        # Predict on the testing data
-        # Your output should be a pandas series, numpy array, or python iterable
-        # where the array elements are floats or bools
-        predictions = my_model.predict(test_inputs)
+        # A single configuration is used
+        params = {'note': 'single config; see benchmark user metadata'}
 
-        # Record your data!
-        task.record(fold, predictions)
+        task.record(fold, predictions, params=params)
 
 # Save your results
-mb.to_file("my_models_benchmark.json.gz")
+mb.to_file("results.json.gz")
 
 
