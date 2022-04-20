b9f08165ceeb7d6f4ea7ff6a73899f572b5f2ac4
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-04-20 21:08:12 +0100

    add __getstate__, __setstate__ magic methods to RecursiveDotDict (fixes #129)

--- matbench/util.py
+++ matbench/util.py
@@ -47,6 +47,12 @@ class RecursiveDotDict(dict):
 
     __setattr__, __getattr__ = __setitem__, __getitem__
 
+    def __getstate__(self):
+        return self.__dict__
+
+    def __setstate__(self, d):
+        self.__dict__.update(d)
+
 
 class MSONable2File:
     """
