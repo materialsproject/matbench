88f467c9b5e8ec6a0a71bd8e0fdc0a0a4db03211
Robin Ruff <robin.ruff.98@googlemail.com>
2023-03-14 12:49:50 +0100

    fix #137

--- matbench/tests/test_data_ops.py
+++ matbench/tests/test_data_ops.py
@@ -5,7 +5,6 @@ from pymatgen.core import Structure
 
 from matbench.constants import CLF_KEY, REG_KEY
 from matbench.data_ops import (
-    homogenize_clf_array,
     load,
     mean_absolute_percentage_error,
     score_array,
@@ -54,13 +53,23 @@ class TestDataOps(unittest.TestCase):
             )
             self.assertIn(df[metadata["target"]].dtypes, test_types)
 
+    def assertDictAlmostEqual(self, first, second, **kwargs):
+        self.assertIsInstance(first, dict)
+        self.assertIsInstance(second, dict)
+        self.assertEqual(first.keys(), second.keys())
+        for key, value in first.items():
+            if isinstance(value, dict):
+                self.assertDictAlmostEqual(value, second[key], **kwargs)
+            else:
+                self.assertAlmostEqual(value, second[key], **kwargs)
+
     def test_score_array(self):
         # test for regression
         true = [1, 2, 3, 4]
         test = [1, 3, 3, 4]
         ans = score_array(true, test, task_type=REG_KEY)
         true_ans = {"mae": 0.25, "rmse": 0.5, "mape": 0.125, "max_error": 1}
-        self.assertDictEqual(ans, true_ans)
+        self.assertDictAlmostEqual(ans, true_ans)
 
         # test for classification
         true = [True, False]
@@ -72,13 +81,19 @@ class TestDataOps(unittest.TestCase):
             "f1": 0.6666666666666666,
             "rocauc": 0.5,
         }
-        self.assertDictEqual(ans, true_ans)
+        self.assertDictAlmostEqual(ans, true_ans)
 
         # test for probability clf
         true = [True, False]
         test = [0.7, 0.65]
         ans = score_array(true, test, task_type=CLF_KEY)
-        self.assertDictEqual(ans, true_ans)
+        true_ans = {
+            "accuracy": 0.5,
+            "balanced_accuracy": 0.5,
+            "f1": 0.6666666666666666,
+            "rocauc": 1.0,
+        }
+        self.assertDictAlmostEqual(ans, true_ans)
 
     def test_mean_absolute_percentage_error(self):
 
@@ -91,25 +106,3 @@ class TestDataOps(unittest.TestCase):
         self.assertAlmostEqual(mape, 0.09999999999999999)
         self.assertAlmostEqual(mape, mape_masked)
 
-    def test_homogenize_clf_array(self):
-
-        bools = [True, False, True, True]
-        floats = [1.0, 0.3, 0.5001, 0.9]
-
-        probs = homogenize_clf_array(bools, to_probs=True)
-        self.assertAlmostEqual(probs[0], 1.0, places=5)
-        self.assertAlmostEqual(probs[1], 0.0, places=5)
-        self.assertAlmostEqual(probs[2], 1.0, places=5)
-        self.assertAlmostEqual(probs[3], 1.0, places=5)
-
-        labels = homogenize_clf_array(floats, to_labels=True, thresh=0.5)
-        self.assertTrue(labels[0])
-        self.assertFalse(labels[1])
-        self.assertTrue(labels[2])
-        self.assertTrue(labels[3])
-
-        labels2 = homogenize_clf_array(floats, to_labels=True, thresh=0.91)
-        self.assertTrue(labels2[0])
-        self.assertFalse(labels2[1])
-        self.assertFalse(labels2[2])
-        self.assertFalse(labels2[3])
