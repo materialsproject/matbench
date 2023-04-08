27c8b7834115b4bce871556317c0992e9af407c9
Hrushikesh Sahasrabuddhe <111614145+hrushikesh-s@users.noreply.github.com>
2023-04-07 21:41:46 -0700

    Add files via upload

--- scripts/rebuild_docs.py
+++ scripts/rebuild_docs.py
@@ -305,7 +305,7 @@ def generate_general_purpose_leaderboard_and_plot(gp_leaderboard_data_by_bmark, 
                   
 
     # Define the table content as a nested list
-    table_header = "|             | [Materials Properties](/#leaderboard-property-general-purpose-algorithms-on-matbench_v01) | [Materials Discovery](/#leaderboard-discovery-general-purpose-algorithms-on-matbench_discovery-010) |\n" \
+    table_header = "|             | [<span style='color: #1e90ff'>Materials Properties</span>](/#leaderboard-property-general-purpose-algorithms-on-matbench_v01) | [<span style='color: #1e90ff'>Materials Discovery</span>](/#leaderboard-discovery-general-purpose-algorithms-on-matbench_discovery-010) |\n" \
                "|-------------|---------------------|---------------------|\n"
 
     table_rows = f"| Task Submissions |        `{n_tasks}`          |        `{n_tasks_discovery}`          |\n" \
